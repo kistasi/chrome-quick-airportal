94d06653d77d050b788fc59d5f6795c09ece3a68
Marton Tasnadi <marton.tasnadi@gmail.com>
2023-01-04 02:43:06 +0100

    Implement functionality

--- popup.js
+++ popup.js
@@ -1,1 +1,43 @@
-console.log('hello world');
+async function getArticles() {
+  const url = 'https://airportal.hu/feed/';
+
+  let response = await fetch(url);
+  response = await response.text();
+  response = new DOMParser().parseFromString(response, 'text/xml');
+
+  return response;
+}
+
+function transformArticlesToJson(articles) {
+  const items = articles.querySelectorAll('rss channel item');
+  let results = [];
+
+  items.forEach(item => {
+    const link = item.querySelector('link').textContent;
+
+    if (!link.includes('/allas-')) {
+      results.push({
+        title: item.querySelector('title').textContent,
+        link: link,
+        description: item.querySelector('description').textContent,
+      });
+    }
+  });
+
+  return results;
+}
+
+function renderArticles(articles) {
+  let html = '';
+  articles.forEach(article => {
+    html += `<li><a target="_blank" href="${article.link}">${article.title}</a></li>`;
+  });
+
+  document.querySelector('.articles ul').innerHTML = html;
+}
+
+(async () => {
+  let articles = await getArticles();
+  articles = transformArticlesToJson(articles);
+  renderArticles(articles);
+})();
